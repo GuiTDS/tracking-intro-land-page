64d2bfa4b9418e02b362515b4005bf9e563ec126
Guilherme Sanches <guitdsanches@gmail.com>
2023-09-19 19:44:16 -0300

    feat: adding background-image

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -4,33 +4,37 @@ import hamburguerLogo from '../assets/images/icon-hamburger.svg';
 import closeIcon from '../assets/images/icon-close.svg';
 import { useState } from 'react';
 import classNames from 'classnames';
-
+import ilustration from '../assets/images/illustration-devices.svg';
 const MainPage = () => {
     const [isOpen, setIsOpen] = useState(false);
     const icon = isOpen ? closeIcon : hamburguerLogo;
-    
-    
+
+
 
     return (
         <>
-            <nav className={styles.navigation}>
-                <img src={logo} alt='Logo' className={styles.navigation__logo} />
+            <div className={styles.header__navigation}>
+                <img src={ilustration} alt='Background illustration' id={styles.ilustration} />
+                <nav className={styles.navigation}>
+                    <img src={logo} alt='Logo' className={styles.navigation__logo} />
 
-                <div>
-                    <img src={icon} alt='hamburguer icon' className={styles.navigation__icon} onClick={() => setIsOpen(!isOpen)}/>
-                    <ul className={classNames({
-                        [styles.navigation__list]:true,
-                        [styles[`navigation__list-active`]]: isOpen,
-                    })}>
-                        <li>PRODUCT</li>
-                        <li>FEATURES</li>
-                        <li>PRINCING</li>
-                        <hr></hr>
-                        <li>LOGIN</li>
-                    </ul>
-                </div>
+                    <div>
+                        <img src={icon} alt='hamburguer icon' className={styles.navigation__icon} onClick={() => setIsOpen(!isOpen)} />
+                        <ul className={classNames({
+                            [styles.navigation__list]: true,
+                            [styles[`navigation__list-active`]]: isOpen,
+                        })}>
+                            <li>PRODUCT</li>
+                            <li>FEATURES</li>
+                            <li>PRINCING</li>
+                            <hr></hr>
+                            <li>LOGIN</li>
+                        </ul>
+                    </div>
 
-            </nav>
+                </nav>
+            </div>
+
         </>
     );
 }
