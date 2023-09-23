b8dd2d7b9b5a02f7ce6e33d05735655d886a23c5
Guilherme Sanches <guitdsanches@gmail.com>
2023-09-23 18:04:21 -0300

    feat: finishing desktop version

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -12,44 +12,42 @@ const MainPage = () => {
 
     return (
         <>
-            <div className={styles.container__navigation}>
 
-                <nav className={styles.navigation}>
-                    <img src={logo} alt='Logo' className={styles.navigation__logo} />
 
-                    <div>
-                        <img src={icon} alt='hamburguer icon' className={styles.navigation__icon} onClick={() => setIsOpen(!isOpen)} />
-                        <ul className={classNames({
-                            [styles.navigation__list]: true,
-                            [styles[`navigation__list-active`]]: isOpen,
-                        })}>
-                            <li>PRODUCT</li>
-                            <li>FEATURES</li>
-                            <li>PRINCING</li>
-                            <hr></hr>
-                            <li>LOGIN</li>
-                        </ul>
-                    </div>
-                </nav>
+            <nav className={styles.navigation}>
+                <img src={logo} alt='Logo' className={styles.navigation__logo} />
+
+                <div>
+                    <img src={icon} alt='hamburguer icon' className={styles.navigation__icon} onClick={() => setIsOpen(!isOpen)} />
+                    <ul className={classNames({
+                        [styles.navigation__list]: true,
+                        [styles[`navigation__list-active`]]: isOpen,
+                    })}>
+                        <li>PRODUCT</li>
+                        <li>FEATURES</li>
+                        <li>PRINCING</li>
+                        <hr></hr>
+                        <li>LOGIN</li>
+                    </ul>
+                </div>
+            </nav>
+            <div className={styles.grid__container}>
                 <div className={styles.navigation__background}>
-                    <img src={ilustration} alt='Background illustration' className={styles.ilustration}/>
+                    <img src={ilustration} alt='Background illustration' className={styles.ilustration} />
                 </div>
 
-
+                <section className={styles.info}>
+                    <p><span>NEW</span> MONOGRAPH DASHBOARD</p>
+                    <h1>POWERFUL INSIGHTS INTO YOUR TEAM</h1>
+                    <p className={styles.info__style}><span>Project planning and time tracking </span>for agile teams</p>
+                    <div className={styles.info__button}>
+                        <button>
+                            SCHEDULE A DEMO
+                        </button>
+                        <p>TO SEE A PREVIEW</p>
+                    </div>
+                </section>
             </div>
-
-            <section className={styles.info}>
-                <p><span>NEW</span> MONOGRAPH DASHBOARD</p>
-                <h1>POWERFUL INSIGHTS INTO YOUR TEAM</h1>
-                <p className={styles.info__style}>Project planning and time tracking for agile teams</p>
-                <div className={styles.info__button}>
-                    <button>
-                        SCHEDULE A DEMO
-                    </button>
-                    <p>TO SEE A PREVIEW</p>
-                </div>
-            </section>
-
         </>
     );
 }
